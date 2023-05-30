2fd32105a33a3a7692469a5c43cf157fa3079ec7
Gregor Gran <grzegorz.swierk01@gmail.com>
2023-05-30 13:23:50 +0200

    Add project files.

---
+++ PracaZLiczbami/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Numerics;
+
+Console.WriteLine("Praca z wielkimi liczbami");
+Console.WriteLine("_________________________");
+ulong wielka = ulong.MaxValue;
+Console.WriteLine($"{wielka,40:N0}");
+
+BigInteger wieksza = BigInteger.Parse("6723673276641941419499426");
+Console.WriteLine($"{wieksza, 40:N0}");
